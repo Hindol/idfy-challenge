1ad97c779af08f05fd5803f353596ba83c18ae26
Hindol Adhya <hindol.adhya@gmail.com>
2015-11-21 11:27:35 +0530

    Initial cleanup after cloning angular-seed

--- app/components/version/version.js
+++ app/components/version/version.js
@@ -1,8 +1,8 @@
 'use strict';
 
-angular.module('myApp.version', [
-  'myApp.version.interpolate-filter',
-  'myApp.version.version-directive'
+angular.module('status-hub.version', [
+  'status-hub.version.interpolate-filter',
+  'status-hub.version.version-directive'
 ])
 
 .value('version', '0.1');
--- app/home/home_test.js
+++ app/home/home_test.js
@@ -1,8 +1,8 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('status-hub.home module', function() {
 
-  beforeEach(module('myApp.view1'));
+  beforeEach(module('status-hub.home'));
 
   describe('view1 controller', function(){
 
